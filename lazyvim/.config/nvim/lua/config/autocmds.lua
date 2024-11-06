158de9173aa9eb1e37821a97014c28b19285680d
horiondreher <horiondreher@gmail.com>
2024-11-06 18:31:09 -0300

    lazyvim kitty and tmux config

--- lazyvim/.config/nvim/lua/config/autocmds.lua
+++ lazyvim/.config/nvim/lua/config/autocmds.lua
@@ -4,23 +4,23 @@
 
 -- Turn off paste mode when leaving insert
 vim.api.nvim_create_autocmd("InsertLeave", {
-  pattern = "*",
-  command = "set nopaste",
+	pattern = "*",
+	command = "set nopaste",
 })
 
 -- Disable the concealing in some file formats
 -- The default conceallevel is 3 in LazyVim
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "json", "jsonc", "markdown" },
-  callback = function()
-    vim.opt.conceallevel = 0
-  end,
+	pattern = { "json", "jsonc", "markdown" },
+	callback = function()
+		vim.opt.conceallevel = 0
+	end,
 })
 
 -- Set guicursor to a:ver30 when leaving vim
 vim.api.nvim_create_autocmd("VimLeave", {
-  pattern = "*",
-  callback = function()
-    vim.o.guicursor = "a:ver30"
-  end,
+	pattern = "*",
+	callback = function()
+		vim.o.guicursor = "a:ver30"
+	end,
 })
--- lazyvim/.config/nvim/lua/config/keymaps.lua
+++ lazyvim/.config/nvim/lua/config/keymaps.lua
@@ -17,7 +17,7 @@ keymap.set("n", "-", "<C-x>")
 keymap.set("n", "dw", 'vb"_d')
 
 -- Select all
-keymap.set("n", "<C-a>", "gg<S-v>G")
+-- keymap.set("n", "<C-a>", "gg<S-v>G")
 
 -- Jumplist
 keymap.set("n", "<C-m>", "<C-i>", opts)
