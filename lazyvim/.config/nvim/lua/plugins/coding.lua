324bea433aa20858c27a8575c068063396e48585
horiondreher <horiondreher@gmail.com>
2025-02-04 00:00:05 -0300

    removing unnecessary

--- lazyvim/.config/nvim/lua/plugins/coding.lua
+++ lazyvim/.config/nvim/lua/plugins/coding.lua
@@ -1,27 +1,4 @@
 return {
-	-- Create annotations with one keybind, and jump your cursor in the inserted annotation
-	{
-		"danymat/neogen",
-		keys = {
-			{
-				"<leader>cc",
-				function()
-					require("neogen").generate({})
-				end,
-				desc = "Neogen Comment",
-			},
-		},
-		opts = { snippet_engine = "luasnip" },
-	},
-
-	-- Incremental rename
-	{
-		"smjonas/inc-rename.nvim",
-		cmd = "IncRename",
-		config = true,
-	},
-
-	-- Refactoring tool
 	{
 		"ThePrimeagen/refactoring.nvim",
 		keys = {
